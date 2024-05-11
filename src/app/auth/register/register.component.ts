611f844832614be232649495a57a252868fffe4b
zentaker <apalomino12@icloud.com>
2024-05-11 18:33:31 -0500

    update

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styles: ``
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  registroForm!: FormGroup;
+  constructor(private fb: FormBuilder){}
+
+
+  ngOnInit(){
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['123@gmail.com', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+
+    })
+
+  } 
+
+
+  onSubmit() {
+    // Verifica si el formulario es válido
+    if (this.registroForm.valid) {
+      console.log(this.registroForm.value);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 
 }
+
+
+
